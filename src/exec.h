b2a0a9a208e670e7479dc84072107a03f71df09d
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-13 18:45:22 -0700

    add public interface

--- src/exec.h
+++ src/exec.h
@@ -1,11 +1,16 @@
 #ifndef _EXEC_H__
 #define _EXEC_H__
 
+#include <types.h>
+
 typedef struct {
 	int pfd;
 	int child_pid;
 } ExecHandle;
 
 int exec_pipe(const char *command, char *argv[], ExecHandle *handle);
+int handle_wait_pid(ExecHandle *handle);
+int64_t handle_write(ExecHandle *handle, const char *buf, size_t len);
+int handle_close(ExecHandle *handle);
 
 #endif	// _EXEC_H__

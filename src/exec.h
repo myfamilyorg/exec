7e8b194f0a76b1dbb6644daa95d3fbe186770cee
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-13 18:39:58 -0700

    remove comment

--- src/exec.h
+++ src/exec.h
@@ -6,10 +6,6 @@ typedef struct {
 	int child_pid;
 } ExecHandle;
 
-/*
-int exec_pipe(const char *command, char *argv[], int *pipefd_out,
-	      int *child_pid);
-	      */
 int exec_pipe(const char *command, char *argv[], ExecHandle *handle);
 
 #endif	// _EXEC_H__
